e0a145e787bf9a927e7fc7d7b2ca1c4f204537fe
Askie01 <adam.paw01@gmail.com>
2024-08-09 21:46:36 +0200

    Initialize repository.
    Initialized repository with spring boot project files.

---
+++ src/main/java/com/askie01/recipeapp/RecipeApplication.java
@@ -0,0 +1,13 @@
+package com.askie01.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/askie01/recipeapp/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.askie01.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

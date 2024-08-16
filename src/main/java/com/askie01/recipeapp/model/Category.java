63c84754555fd49b8cc5a78fb6e4d9132fbf2e15
Askie01 <adam.paw01@gmail.com>
2024-08-16 20:42:46 +0200

    Create project's entities.
    * Created all entity classes for this project to work.
    * We can create recipes, define difficulty, provide notes, ingredients with unit measures and categorize recipes.

---
+++ src/main/java/com/askie01/recipeapp/model/Category.java
@@ -0,0 +1,22 @@
+package com.askie01.recipeapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+}
---
+++ src/main/java/com/askie01/recipeapp/model/Note.java
@@ -0,0 +1,23 @@
+package com.askie01.recipeapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String note;
+}

873911e6056451577b955f538bf9a2e4ec764fc8
Askie01 <adam.paw01@gmail.com>
2024-08-16 20:41:24 +0200

    Remove @RequestMapping in 'HomeController'.
    * Removed @RequestMapping("") annotation - because there's no need for no-specific path controller to have such annotation.
    * This commit is cleanup kind.

--- src/main/java/com/askie01/recipeapp/controller/HomeController.java
+++ src/main/java/com/askie01/recipeapp/controller/HomeController.java
@@ -2,10 +2,8 @@ package com.askie01.recipeapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = "")
 public class HomeController {
 
     @GetMapping(path = "home")

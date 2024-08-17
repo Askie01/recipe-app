eea6f30dc0e77c03a0ada080ec18cf730f132dd9
Askie01 <adam.paw01@gmail.com>
2024-08-17 18:36:12 +0200

    Simplify repositories.
    * Simplified repositories - They will be continuously updated with further updates.
    * However we need to create few tasks first.

--- src/main/java/com/askie01/recipeapp/repository/RecipeRepository.java
+++ src/main/java/com/askie01/recipeapp/repository/RecipeRepository.java
@@ -1,9 +1,9 @@
 package com.askie01.recipeapp.repository;
 
 import com.askie01.recipeapp.model.Recipe;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 }

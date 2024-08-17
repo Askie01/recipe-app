4d6f2e9a1bb441c0e8fae55c4058c457f47358b2
Askie01 <adam.paw01@gmail.com>
2024-08-17 09:56:13 +0200

    Display 'Category' & 'MeasureUnit' in console when calling home page.
    * Displayed 'American' category & 'teaspoon' measure unit from the database & displayed it in the console.
    * This commit is a small test - to check if repositories are working correctly.

--- src/main/java/com/askie01/recipeapp/controller/HomeController.java
+++ src/main/java/com/askie01/recipeapp/controller/HomeController.java
@@ -1,13 +1,37 @@
 package com.askie01.recipeapp.controller;
 
+import com.askie01.recipeapp.exception.ResourceNotFoundException;
+import com.askie01.recipeapp.model.Category;
+import com.askie01.recipeapp.model.MeasureUnit;
+import com.askie01.recipeapp.repository.CategoryRepository;
+import com.askie01.recipeapp.repository.MeasureUnitRepository;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Data
 @Controller
 public class HomeController {
 
+    private final CategoryRepository categoryRepository;
+    private final MeasureUnitRepository measureUnitRepository;
+
     @GetMapping(path = "home")
     public String getHomePage() {
+        final Category americanCategory = categoryRepository
+                .findByName("American")
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("'American' category does not exist.")
+                );
+        System.out.println(americanCategory);
+
+        final MeasureUnit teaspoonMeasureUnit = measureUnitRepository
+                .findByName("teaspoon")
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("'teaspoon' measure unit does not exist.")
+                );
+
+        System.out.println(teaspoonMeasureUnit);
         return "home";
     }
 }

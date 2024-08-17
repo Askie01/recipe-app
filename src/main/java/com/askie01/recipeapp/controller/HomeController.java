cdbf05d230d04f97034b3558d230a8215654a2a0
Askie01 <adam.paw01@gmail.com>
2024-08-17 18:19:50 +0200

    Update 'HomeController'
    * Removed repositories in 'HomeController' - because this controller doesn't use them anyway. In the future, he will use service layers to get data & optionally apply them to 'Model' at the site.

--- src/main/java/com/askie01/recipeapp/controller/HomeController.java
+++ src/main/java/com/askie01/recipeapp/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.askie01.recipeapp.controller;
 
-import com.askie01.recipeapp.repository.CategoryRepository;
-import com.askie01.recipeapp.repository.MeasureUnitRepository;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Data
 @Controller
 public class HomeController {
-
-    private final CategoryRepository categoryRepository;
-    private final MeasureUnitRepository measureUnitRepository;
 
     @GetMapping(path = "home")
     public String getHomePage() {

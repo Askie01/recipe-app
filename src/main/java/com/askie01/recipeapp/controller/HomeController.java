6b8c06ec54691ae086078a44117005ccaf1227a2
Askie01 <adam.paw01@gmail.com>
2024-08-17 12:02:54 +0200

    Update 'HomeController'.
    * Removed previously added test - Controllers should not be used to test things.

--- src/main/java/com/askie01/recipeapp/controller/HomeController.java
+++ src/main/java/com/askie01/recipeapp/controller/HomeController.java
@@ -1,8 +1,5 @@
 package com.askie01.recipeapp.controller;
 
-import com.askie01.recipeapp.exception.ResourceNotFoundException;
-import com.askie01.recipeapp.model.Category;
-import com.askie01.recipeapp.model.MeasureUnit;
 import com.askie01.recipeapp.repository.CategoryRepository;
 import com.askie01.recipeapp.repository.MeasureUnitRepository;
 import lombok.Data;
@@ -18,20 +15,6 @@ public class HomeController {
 
     @GetMapping(path = "home")
     public String getHomePage() {
-        final Category americanCategory = categoryRepository
-                .findByName("American")
-                .orElseThrow(
-                        () -> new ResourceNotFoundException("'American' category does not exist.")
-                );
-        System.out.println(americanCategory);
-
-        final MeasureUnit teaspoonMeasureUnit = measureUnitRepository
-                .findByName("teaspoon")
-                .orElseThrow(
-                        () -> new ResourceNotFoundException("'teaspoon' measure unit does not exist.")
-                );
-
-        System.out.println(teaspoonMeasureUnit);
         return "home";
     }
 }

711f7c88b0c8a28a83ad8c8f1d02d41819ee8c0b
Askie01 <adam.paw01@gmail.com>
2024-08-26 16:30:27 +0200

    Create Recipe, Category & Ingredient mappers.
    * There might be slight inconvenience in 'RecipeMapper' for mapping RecipeDTO to Recipe & vice versa - because same method names are called, only source swapped with target.
    * IDE detects duplication there - but for now I have no ideas how to deal with that.

---
+++ src/main/java/com/askie01/recipeapp/mapper/CategoryMapper.java
@@ -0,0 +1,30 @@
+package com.askie01.recipeapp.mapper;
+
+import com.askie01.recipeapp.model.Category;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CategoryMapper {
+    public static Set<Category> mapToCategory(Set<String> categoryNames) {
+        final Set<Category> categories = new HashSet<>();
+        categoryNames.forEach(categoryName -> {
+            final Category category = new Category();
+            category.setName(categoryName);
+            categories.add(category);
+        });
+        return categories;
+    }
+
+    public static Set<String> mapToCategoryNames(Set<Category> categories) {
+        final Set<String> categoryNames = new HashSet<>();
+        categories.forEach(category -> {
+            final String name = category.getName();
+            categoryNames.add(name);
+        });
+        return categoryNames;
+    }
+}
--- src/main/java/com/askie01/recipeapp/mapper/RecipeMapper.java
+++ src/main/java/com/askie01/recipeapp/mapper/RecipeMapper.java
@@ -9,56 +9,119 @@ import com.askie01.recipeapp.model.Recipe;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.util.HashSet;
 import java.util.Set;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RecipeMapper {
 
-    public static Recipe mapToRecipe(RecipeDTO recipeDTO, Recipe recipe) {
-        recipe.setName(recipeDTO.getName());
-        recipe.setDescription(recipeDTO.getDescription());
-        recipe.setServings(recipeDTO.getServings());
-        recipe.setPreparationTime(recipeDTO.getPreparationTime());
-        recipe.setInstructions(recipeDTO.getInstructions());
-        recipe.setDifficultyLevel(mapToDifficultyLevel(recipeDTO.getDifficultyLevel()));
-        recipe.setCategories(mapToCategories(recipeDTO.getCategories()));
-        recipe.setIngredients(mapToIngredients(recipeDTO.getIngredients()));
-        return recipe;
+    public static Recipe mapToRecipe(RecipeDTO source, Recipe target) {
+        mapName(source, target);
+        mapDescription(source, target);
+        mapServings(source, target);
+        mapPreparationTime(source, target);
+        mapInstructions(source, target);
+        mapDifficultyLevel(source, target);
+        mapCategories(source, target);
+        mapIngredients(source, target);
+        return target;
     }
 
-    //TODO - Move this function to separate Mapper class. Something called "CategoryMapper"
-    private static Set<Category> mapToCategories(Set<String> categoryStrings) {
-        final Set<Category> categories = new HashSet<>();
-        categoryStrings.forEach(categoryName -> {
-            final Category category = new Category();
-            category.setName(categoryName);
-            categories.add(category);
-        });
-        return categories;
+    private static void mapName(RecipeDTO source, Recipe target) {
+        final String name = source.getName();
+        target.setName(name);
     }
 
-    //TODO - Move this function to separate Mapper class. Something called "IngredientMapper"
-    private static Set<Ingredient> mapToIngredients(Set<IngredientDTO> ingredientDTOs) {
-        final Set<Ingredient> ingredients = new HashSet<>();
-        ingredientDTOs.forEach(ingredientDTO -> {
-            final Ingredient ingredient = new Ingredient();
-            ingredient.setName(ingredientDTO.getName());
-            ingredient.setAmount(ingredientDTO.getAmount());
-            ingredients.add(ingredient);
-        });
-        return ingredients;
+    private static void mapDescription(RecipeDTO source, Recipe target) {
+        final String description = source.getDescription();
+        target.setDescription(description);
     }
 
-    private static DifficultyLevel mapToDifficultyLevel(String difficultyLevelString) {
-        return DifficultyLevel.valueOf(difficultyLevelString.toUpperCase());
+    private static void mapServings(RecipeDTO source, Recipe target) {
+        final Integer servings = source.getServings();
+        target.setServings(servings);
     }
 
-    //TODO - Create mapping to RecipeDTO from Recipe object
-    // This object will be displayed with the get request in the future.
-//    public static RecipeDTO mapToRecipeDTO(Recipe recipe, RecipeDTO recipeDTO) {
-//        recipeDTO.setName(recipe.getName());
-//        recipeDTO.setDescription(recipe.getDescription());
-//        recipeDTO.setServings(recipe.getServings());
-//    }
+    private static void mapPreparationTime(RecipeDTO source, Recipe target) {
+        final Integer preparationTime = source.getPreparationTime();
+        target.setPreparationTime(preparationTime);
+    }
+
+    private static void mapInstructions(RecipeDTO source, Recipe target) {
+        final String instructions = source.getInstructions();
+        target.setInstructions(instructions);
+    }
+
+    private static void mapDifficultyLevel(RecipeDTO source, Recipe target) {
+        final String difficultyLevelString = source.getDifficultyLevel();
+        final String difficultyLevelStringToUpperCase = difficultyLevelString.toUpperCase();
+        final DifficultyLevel difficultyLevel = DifficultyLevel.valueOf(difficultyLevelStringToUpperCase);
+        target.setDifficultyLevel(difficultyLevel);
+    }
+
+    private static void mapCategories(RecipeDTO source, Recipe target) {
+        final Set<String> categoryStrings = source.getCategories();
+        final Set<Category> categories = CategoryMapper.mapToCategory(categoryStrings);
+        target.setCategories(categories);
+    }
+
+    private static void mapIngredients(RecipeDTO source, Recipe target) {
+        final Set<IngredientDTO> ingredientDTOS = source.getIngredients();
+        final Set<Ingredient> ingredients = IngredientMapper.mapToIngredients(ingredientDTOS);
+        target.setIngredients(ingredients);
+    }
+
+    public static RecipeDTO mapToRecipeDTO(Recipe source, RecipeDTO target) {
+        mapName(source, target);
+        mapDescription(source, target);
+        mapServings(source, target);
+        mapPreparationTime(source, target);
+        mapInstructions(source, target);
+        mapDifficultyLevel(source, target);
+        mapCategories(source, target);
+        mapIngredients(source, target);
+        return target;
+    }
+
+    private static void mapName(Recipe source, RecipeDTO target) {
+        final String name = source.getName();
+        target.setName(name);
+    }
+
+    private static void mapDescription(Recipe source, RecipeDTO target) {
+        final String description = source.getDescription();
+        target.setDescription(description);
+    }
+
+    private static void mapServings(Recipe source, RecipeDTO target) {
+        final Integer servings = source.getServings();
+        target.setServings(servings);
+    }
+
+    private static void mapPreparationTime(Recipe source, RecipeDTO target) {
+        final Integer preparationTime = source.getPreparationTime();
+        target.setPreparationTime(preparationTime);
+    }
+
+    private static void mapInstructions(Recipe source, RecipeDTO target) {
+        final String instructions = source.getInstructions();
+        target.setInstructions(instructions);
+    }
+
+    private static void mapDifficultyLevel(Recipe source, RecipeDTO target) {
+        final DifficultyLevel difficultyLevel = source.getDifficultyLevel();
+        final String difficultyLevelString = difficultyLevel.toString();
+        target.setDifficultyLevel(difficultyLevelString);
+    }
+
+    private static void mapCategories(Recipe source, RecipeDTO target) {
+        final Set<Category> categories = source.getCategories();
+        final Set<String> categoryNames = CategoryMapper.mapToCategoryNames(categories);
+        target.setCategories(categoryNames);
+    }
+
+    private static void mapIngredients(Recipe source, RecipeDTO target) {
+        final Set<Ingredient> ingredients = source.getIngredients();
+        final Set<IngredientDTO> ingredientDTOS = IngredientMapper.mapToIngredientDTOs(ingredients);
+        target.setIngredients(ingredientDTOS);
+    }
 }

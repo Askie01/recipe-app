2af2f25c64a1536a7519cfb35080145d2328782d
Askie01 <adam.paw01@gmail.com>
2024-09-01 00:26:42 +0200

    Update 'Category', 'Ingredient' & 'Recipe'.
    * Updated 'Category' & 'Ingredient' toString method which calls super toString method.
    * Updated 'Recipe' ingredients fetch type to Eager - This is needed to remove old ingredients later on.

--- src/main/java/com/askie01/recipeapp/model/Category.java
+++ src/main/java/com/askie01/recipeapp/model/Category.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @Entity
 @NoArgsConstructor
+@ToString(callSuper = true)
 @Table(name = "categories")
 @EqualsAndHashCode(callSuper = true)
 public class Category extends NamedEntity {
